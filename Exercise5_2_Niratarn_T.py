b2ae4f412207b8ee1be6e13f9c01d1748fbaa109
zangiii <niratarn@gmail.com>
2022-07-29 00:11:29 -0700

    delete r

--- Exercise5_2_Niratarn_T.py
+++ Exercise5_2_Niratarn_T.py
@@ -3,11 +3,11 @@ print("-----Average Velocity Calculator-----")
 
 s = int(input("Displacement(km) = "))
 print("s =" ,s , "km")
-t = int(input("Time(hr) = "))
+t = int(input("Time(h) = "))
 print("t =" ,t , "hr")
-print("Average Velocity = v = s/t","km/hr")
+print("Average Velocity = v = s/t","km/h")
 v = s/t
-print( "                  v =",v  ,"km/hr")
+print( "                  v =",v  ,"km/h")
 
 
 
