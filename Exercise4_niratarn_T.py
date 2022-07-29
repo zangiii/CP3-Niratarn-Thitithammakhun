0aca0fc92c2588691651cd0c63eb34564567b52f
zangiii <niratarn@gmail.com>
2022-07-28 17:00:46 -0700

    Create Exercise4_niratarn_T.py

---
+++ Exercise4_niratarn_T.py
@@ -0,0 +1,16 @@
+FoundationEnglish = 78
+GeneralBusiness = 89.9
+IntroductionComputerSystems = 100
+ComputerProgramming = 88
+
+print("                --- You Score ---")
+
+print("Foundation English                : ",FoundationEnglish )
+print("General Business                  : ",GeneralBusiness   )
+print("Introduction to Computer Systems  : ",IntroductionComputerSystems )
+print("Computer Programming              : ",FoundationEnglish )
+ 
+
+
+
+
